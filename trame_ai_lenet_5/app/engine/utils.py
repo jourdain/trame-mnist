6f0e8ee487a6548b19e0d3a33b7610511873083a
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-02-09 13:43:35 -0700

    small cleanup

--- trame_ai_lenet_5/app/engine/utils.py
+++ trame_ai_lenet_5/app/engine/utils.py
@@ -1,10 +1,8 @@
 import asyncio
 from trame import state
-from trame.internal.app import get_app_instance
 
 
 async def monitor_state_queue(queue, training_task):
-    _app = get_app_instance()
     _process_running = True
     while _process_running:
         if queue.empty():
@@ -16,9 +14,9 @@ async def monitor_state_queue(queue, training_task):
                 if msg == "stop":
                     _process_running = False
             else:
-                # state update (dict)
+                # Need to monitor as we are outside of client/server update
                 with state.monitor():
-                    # Need to monitor as we are outside of client/server update
+                    # state update (dict)
                     state.update(msg)
 
     await training_task
